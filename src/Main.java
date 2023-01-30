bbea1f78afda81838b856efb54b0f8901381d3e7
Роман <roma_yakushin@mail.ru>
2023-01-30 15:56:45 +0300

    Решение задач 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         task1();
         task2();
+        task3();
     }
-
     public static void task1() {
-        System.out.println("Hello 1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 73789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
-
     public static void task2() {
-        System.out.println("Hello 1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 73789;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+    public static void task3() {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 73789;
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 
 }
