43b0085af2a0b3f1751996bd612f1cb9a6cb326b
Роман <roma_yakushin@mail.ru>
2023-01-30 17:03:26 +0300

    Решение задач 6-8

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
+        task7();
+        task8();
     }
     public static void task1() {
         System.out.println("...Task 1...");
@@ -58,6 +61,35 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+    }
+    public static void task6() {
+        System.out.println("...Task 6...");
+        var box1 = 78.2;
+        var box2 = 82.7;
+        var summa = box1 + box2;
+        var diff = box1 - box2;
+        System.out.println("Общий вес спортсменов - " + summa + " кг");
+        System.out.println("Разница в весе между боксерами " + diff + " кг");
+    }
+    public static void task7() {
+        System.out.println("...Task 7...");
+        var box1 = 78.2;
+        var box2 = 82.7;
+        var diff = box2 - box1;
+        System.out.println("Разница в весе между боксерами " + diff + " кг (способ '-')");
+        diff = box2 % box1;
+        System.out.println("Разница в весе между боксерами " + diff + " кг (спооб '%')");
+    }
+    public static void task8() {
+        System.out.println("...Task 8...");
+        var timeTotal = 640;
+        var timeOnOne = 8;
+        var emp = timeTotal / timeOnOne;
+        System.out.println("Всего работников в компании - " + emp + " человек");
+        emp = emp + 94;
+        timeTotal = emp * timeOnOne;
+        System.out.println("Если в компании работает " + emp + " человек, то всего " + timeTotal + " часа работы может быть поделено между сотрудниками. ");
+
     }
 
 }
