4f37610b0b386a35b5c286c0469dc5d654d1987f
Роман <roma_yakushin@mail.ru>
2023-01-30 16:34:36 +0300

    Решение задач 4-5

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,23 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+        task5();
     }
     public static void task1() {
+        System.out.println("...Task 1...");
         var dog = 8.0;
         var cat = 3.6;
-        var paper = 73789;
+        var paper = 763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
     }
     public static void task2() {
+        System.out.println("...Task 2...");
         var dog = 8.0;
         var cat = 3.6;
-        var paper = 73789;
+        var paper = 763789;
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
@@ -24,15 +28,36 @@ public class Main {
         System.out.println(paper);
     }
     public static void task3() {
+        System.out.println("...Task 3...");
         var dog = 8.0;
         var cat = 3.6;
-        var paper = 73789;
+        var paper = 763789;
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+    }
+    public static void task4() {
+        System.out.println("...Task 4...");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 3;
+        System.out.println(friend);
+    }
+    public  static void task5() {
+        System.out.println("...Task 5...");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 
 }
